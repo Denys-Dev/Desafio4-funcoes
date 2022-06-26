6d219b9e7dbf8cd9f19b83c9457b0f9dba2dd473
Denys-Dev <mrdenys18@gmail.com>
2022-06-26 17:38:20 -0300

    Desafio4-funcao

---
+++ script.js
@@ -0,0 +1,29 @@
+function msg(){
+    console.log("é isso ai");
+    
+}
+
+msg()
+
+function myname(nome){
+    console.log(`bem-vindo(a), ${nome}!` );
+
+}
+myname("Denilson")
+
+function about (name, age, music){
+    console.log(`meu nome é ${name} eu tenho ${age} gosto de ouvir ${music}`);
+}
+about("Denilson", 24, "trap")
+
+function fun( movie, music){
+    console.log(`Meu filme favorito é ${movie} minha musica favorita é ${music}`);
+}
+fun("ate ultimo homen, ela so quer paz")
+
+function calc(triplo){
+    return triplo * 3
+}
+
+const result = calc(10)
+console.log(result);
